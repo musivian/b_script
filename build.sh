b1b99e94ef388861c58ae2838df335480cdbb64f
Demi <kizura.vian@gmail.com>
2024-10-15 20:11:53 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Remove existing local_manifests
+echo "==============================="
+echo "Delete Manifest and Device Tree"
+echo "==============================="
+sudo rm -rf .repo/local_manifests/
+sudo rm -rf device/* vendor/*
+
+#repo init rom
+repo init -u https://github.com/LineageOS/android.git -b lineage-22.0 --git-lfs --depth=1
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+#Local manifests
+git clone https://github.com/musivian/local_manifests -b main .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+/opt/crave/resync.sh || repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Export
+export BUILD_USERNAME="ZN"
+export BUILD_HOSTNAME="crave"
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+lunch lineage_sunny-userdebug
+
+make installclean -j$(nproc --all)
+echo "============="
+
+echo "Build system"
+make bacon
